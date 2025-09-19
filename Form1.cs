08938dfab4a61f4238dc4a03403cd22d99713eb0
HomePC <HomePC@YATOR>
2025-09-19 11:49:10 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using Bunifu.UI.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bunifuDatePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //BunifuDatePicker bunifuDatePicker = new BunifuDatePicker();
+            //bunifuDatePicker.Size = new Size(330, 200);
+            //bunifuDatePicker.Location = new Point(400, 20);
+            //bunifuDatePicker.Color = Color.DodgerBlue;
+            //bunifuDatePicker.IconColor = Color.DodgerBlue;
+            //bunifuDatePicker.BorderRadius = 8;
+            //bunifuDatePicker.DateBorderThickness = BunifuDatePicker.BorderThickness.Thick;
+            //this.Controls.Add(bunifuDatePicker);
+        }
+
+        private void bunifuTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuDatePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuLabel13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuPanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
